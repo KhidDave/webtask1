6d8c00f0c9c278ec685870a8ad13eafcdd2964e8
KhidDave <somadina0405@gmail.com>
2024-07-03 21:59:41 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ WebTask.API/Controllers/helloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class helloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInteraction(string visitor_name)
+        {
+            var clientIP = "127.0.0.1";
+            var location = "New York";
+            var temperatureCelsius = 11;
+
+            var response = new 
+            {
+                clientIP = clientIP,
+                location = location,
+                greetings = $"Hello, {visitor_name}! the temperature is {temperatureCelsius}ºC in {location}"
+            };
+
+            return Ok(response);
+        }
+    }
+}
